f38e74bcee3139abc9cc85a9e1082154eb3df144
Max Nikitin <40297478+ITSalt@users.noreply.github.com>
2023-04-12 17:36:21 +0300

    Update function.js
    Version without internal function

--- function.js
+++ function.js
@@ -4,19 +4,18 @@ const glideamount = in_amount.value;
 const gliderecip = String(in_recip.value) ?? "";
 const glideorder = String(in_order.value) ?? "";
 
+const secret = '7a395ee6-de9e-4225-a82c-04612683ed65';
+const data = {
+  currencyID: 4,
+  amount: glideamount,
+  recipientTypeID: 1,
+  recipient: `${gliderecip}`,
+  clientOrderID: `${glideorder}`,
+  webhookURL: 'https://webhook.site/6c2fa6fa-ec9f-47ca-a2c2-18505a6dec9b'
+};
+const raw = JSON.stringify(data);
+const hash = CryptoJS.HmacSHA512(raw, secret).toString();
 
-  const secret = '7a395ee6-de9e-4225-a82c-04612683ed65';
-  const data = {
-    currencyID: 4,
-    amount: glideamount,
-    recipientTypeID: 1,
-    recipient: `${gliderecip}`,
-    clientOrderID: `${glideorder}`,
-    webhookURL: 'https://webhook.site/6c2fa6fa-ec9f-47ca-a2c2-18505a6dec9b'
-  };
-  const raw = JSON.stringify(data);
-  const hash = CryptoJS.HmacSHA512(raw, secret).toString();
-  return hash;
-
+return hash;
 
 }
