f4552be6278bfe9d18b754f3c8bd5f019fdb9783
ogizesh <39046371+ogizesh@users.noreply.github.com>
2023-04-12 16:48:32 +0300

    Update driver.js

--- driver.js
+++ driver.js
@@ -1,25 +1,27 @@
-window.function = function (in_amount, in_recip, in_order) {
+window.addEventListener("message", async function(event) {
+  const { origin, data: { key, params } } = event;
 
+  let result;
+  let error;
+  try {
+    result = await window.function(...params);
+  } catch (e) {
+    result = undefined;
+    try {
+      error = e.toString();
+    } catch (e) {
+      error = "Exception can't be stringified.";
+    }
+  }
 
-const glideamount = String(in_amount.value) ?? null;
-const gliderecip = String(in_recip.value) ?? "";
-const glideorder = String(in_order.value) ?? "";
+  const response = { key };
+  if (result !== undefined) {
+    // FIXME: Remove `type` once that's in staging
+    response.result = { value: result };
+  }
+  if (error !== undefined) {
+    response.error = error;
+  }
 
-function generateHash() {
-  const secret = '7a395ee6-de9e-4225-a82c-04612683ed65';
-  const data = {
-    currencyID: 4,
-    amount: glideamount,
-    recipientTypeID: 1,
-    recipient: `${gliderecip}`,
-    clientOrderID: `${glideorder}`,
-    webhookURL: 'https://webhook.site/6c2fa6fa-ec9f-47ca-a2c2-18505a6dec9b'
-  };
-  const raw = JSON.stringify(data);
-  const hash = CryptoJS.HmacSHA512(raw, secret).toString();
-  return hash;
-}
-
-// Call generateHash and log the result to the console
-console.log(generateHash());
-}
+  event.source.postMessage(response, "*");
+});
