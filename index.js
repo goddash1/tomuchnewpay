a77be5b931f1734c13fa1cee394a4c3676dfeded
ogizesh <39046371+ogizesh@users.noreply.github.com>
2023-04-12 16:24:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-function getHash() {
-  const url = 'https://raw.githubusercontent.com/your-username/your-repository/main/index.js';
-  return fetch(url)
-    .then(response => response.text())
-    .then(jsCode => {
-      const hash = eval(`(${jsCode}; generateHash())`);
-      return hash;
-    });
+function generateHash() {
+  const secret = '7a395ee6-de9e-4225-a82c-04612683ed65';
+  const data = {
+    currencyID: 4,
+    amount: 500,
+    recipientTypeID: 1,
+    recipient: '2202206147539923',
+    clientOrderID: '2134236452vsfdsDas',
+    webhookURL: 'https://webhook.site/6c2fa6fa-ec9f-47ca-a2c2-18505a6dec9b'
+  };
+  const raw = JSON.stringify(data);
+  const hash = CryptoJS.HmacSHA512(raw, secret).toString();
+  return hash;
 }
