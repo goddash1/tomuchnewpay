e53b8384a28089546bce908eea9de6e1e1c4ab72
goddash1 <133566104+goddash1@users.noreply.github.com>
2023-05-25 12:34:47 +0300

    Update function.js

--- function.js
+++ function.js
@@ -5,7 +5,7 @@ const gliderecip = String(in_recip.value) ?? "";
 const glideorder = String(in_order.value) ?? "";
 
 function generateHash() {
-  const secret = '7a395ee6-de9e-4225-a82c-04612683ed65';
+  const secret = 'nhYFNjbVKLfNGElt5blyDIMQWz4bCfho66uYSE4JakDX58Ri5ZUBW0i9l3L9aTP0A6TD2XhZ2QCsHa8FHr9j0Fog4Id86asUfWHM9mSDQOfSaaBvhogzUScjveqoEJWT7i5Pq1Npy9QT3v4TO4dM6obUzxOCwpoRh9xDbmaLopaenkxjvYQdhNnF0ybJgxxhGbg1Wz1aAVx6kBZRtu9HAxkN5iv35OT75uB8sADGf1juRtnQB2WQuV6cMWfuNSaD';
   const data = {
     currencyID: 4,
     amount: glideamount,
